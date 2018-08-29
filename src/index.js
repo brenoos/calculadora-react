d25549ef9db1c4d5a3fe33471380108d483018db
Breno Oliveira dos Santos <breno.oliver07@gmail.com>
2018-08-29 10:39:26 -0300

    botões e grid

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Calculator from './main/Calculator'
+import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<Calculator />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
